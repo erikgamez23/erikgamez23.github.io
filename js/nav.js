716ca736baf01b3269ee881b4cd9dbae18969be6
Erik Gamez <erikgamez23@gmail.com>
2020-04-30 21:08:25 -0400

    h

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,14 @@
 function openNav() {
-    document.querySelector('#mySidenav').style.width = "300px"; 
+    document.querySelector('#mySidenav').style.width = "15%";
     document.querySelector('.all-over-bkg').classList.add('is-visible');
+    document.querySelector('#mySidenav').style.border = "5px";
 }
-   
+
 function closeNav() {
-    document.querySelector('#mySidenav').style.width = "0"; 
+    document.querySelector('#mySidenav').style.width = "0";
    document.querySelector('.all-over-bkg').classList.remove('is-visible');
+    document.querySelector('#mySidenav').style.border = "0px";
 }
-   
+
 document.querySelector('.openbtn').addEventListener('click', openNav);
 document.querySelector('.closebtn').addEventListener('click', closeNav);
