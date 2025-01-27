9f5948f8fe62a37735d657ba0fd18b726ec80135
erikgamez23 <erikgamez23@gmail.com>
2025-01-26 18:16:55 -0600

    Set up homescreen

--- erikgamezcom/src/App.tsx
+++ erikgamezcom/src/App.tsx
@@ -1,39 +1,22 @@
 // import { useState } from 'react'
-import icon from './assets/favicon/android-chrome-384x384.png'
 import erikHome from './assets/erik/erikCooking.png';
-
+import NavigationBar from './Components/NavigationBar'
 import './App.css'
-import Card from './Card';
+import Card from './Components/Card';
+
 
 function App() {
 
   const title = "Hi, my name is Erik!"
-  const body = ""
+  const body = "This is a work in progress; planning to make this a personal blog/portfolio."
 
   return (
     <>
-      <div>
-        <a href="https://erikgamez.com" target="_blank">
-          <img src={icon} className="logo" alt="Erik Gamez logo" />
-        </a>
-      </div>
+      <NavigationBar></NavigationBar>
 
-      <div className='hstack'>
-        {/* I can add a link to my Cooking Tab later with Recipes? */}
-        <img src={erikHome} className="headerImage" alt="Erik Cooking" />
+      <img src={erikHome} className="headerImage" alt="Erik Cooking" />
 
-        <div className='vstack'>
-        <Card
-            title={title}
-            body={body}
-          ></Card>
-          <Card
-            title={title}
-            body={body}
-          ></Card>
-        </div>
-
-      </div>
+      <Card title={title} body={body}></Card>
 
     </>
   )
---
+++ erikgamezcom/src/Components/NavigationBar.tsx
@@ -0,0 +1,28 @@
+import icon from '../assets/favicon/android-chrome-384x384.png'
+import { AppBar, Avatar, Box, Button, IconButton, Toolbar } from '@mui/material';
+
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar color="transparent" position="fixed">
+        <Toolbar>
+          <a href="https://erikgamez.com" target="_blank">
+            <Avatar sx={{ width: 100, height: 100 }} src={icon}></Avatar>
+          </a>
+
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          ></IconButton>
+
+          <Button color="inherit"> About Me </Button>
+          <Button color="inherit"> Resume </Button>
+          <Button color="inherit"> Music </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
